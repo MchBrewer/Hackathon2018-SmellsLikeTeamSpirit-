e9c7e9020339e2492cd1b249488a26c9c1a5186e
Micah Brewer <micah@wless-user-172019249194.temp.wsu.edu>
2018-03-03 18:04:44 -0800

    Added text box and buttons

--- mainGUI.py
+++ mainGUI.py
@@ -6,8 +6,15 @@ class window(wx.Frame):
         self.Center()
         self.Show()
         
-        self.panel = wx.Panel(self, size=(800,500))
+        self.panel      = wx.Panel  (self,  size=(800,500))
         self.panel.SetBackgroundColour("Cream")
+        
+        self.sigBTN     = wx.Button (self.panel,    label="sigma",  pos=(10,10),    size=(75,25))
+        self.piBTN      = wx.Button (self.panel,    label="pi",     pos=(10, 35),   size=(75,25))
+        
+       #self.funcInput  = wx.TextEntryDialog    (self.panel,    "Promt",    "title",            "DefaultText")
+        self.fuctInput  = wx.TextCtrl(self.panel,   -1,             "f(x)",         pos=(10,55),    size=(150,50))  
+        
         
 def main():
     app = wx.App()
