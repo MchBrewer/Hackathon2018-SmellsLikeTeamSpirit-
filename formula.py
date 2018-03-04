b3ae85fc750062150ab7db453be1161fa4632efe
TheGentlemanCoder <santasmith123@hotmail.com>
2018-03-04 00:59:28 -0800

    Flesh out the Formula object to add translation from markdown notation to Python notation, and add two more formulas to be utilized.

--- formula.py
+++ formula.py
@@ -1,15 +1,47 @@
+import re
+
+class MarkdownError(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, "Invalid markdown expression.")
+
 class Formula(object):
-    def __init__(self, formulaType, parameters, variableNames):
+    def __init__(self, formulaType, parameters, variableNames, markdown):
         self.formulaType = formulaType
         self.parameters = parameters
-        self.lambdaFunction = parameters["_function_"]
+        self.lambdaFunction = getLambdaFunctionFrom(markdown)
+        self.parameters["_function_"] = self.lambdaFunction
         self.code = self.getCodeFor(formulaType, parameters)
-        self.changeParameterNames(variableNames)
     
-    def getLambdaFunctionFrom(userInput):
-        # TODO make this function an interpreter from the syntax used
-        # to display the function and Python syntax
-        pass
+    def getLambdaFunctionFrom(self, markdown):
+        converted_markdown = self.convertSquareRootsIn(markdown)
+        return converted_markdown
+    
+    def convertSquareRootsIn(self, markdown):
+        phrase = "sqrt"
+        converted_markdown = markdown
+        while (converted_markdown.find(phrase) > -1):
+            index = converted_markdown.find(phrase)
+            open_parenthesis_index = index + len(phrase)
+            closed_parenthesis_index = self.findCorrespondingClosedParenthesisFor(open_parenthesis_index, converted_markdown)
+            converted_markdown = converted_markdown[:index] + \
+                                 converted_markdown[open_parenthesis_index:closed_parenthesis_index+1] + \
+                                 "**0.5" + \
+                                 converted_markdown[closed_parenthesis_index+1:]
+        return converted_markdown
+    
+    def findCorrespondingClosedParenthesisFor(self, indexOpenParenthesis, string):
+        value = 0
+        closed_parenthesis_index = indexOpenParenthesis
+        for character_index in xrange(indexOpenParenthesis, len(string)):
+            if string[character_index] == "(":
+                value += 1
+            elif string[character_index] == ")":
+                value -= 1
+                    
+            # value will equal zero when the index of the corresponding closed
+            # parenthesis is found
+            if value == 0:
+                return character_index
     
     def getCodeFor(self, formulaType, parameters):
         if formulaType == "sigma":
@@ -18,12 +50,25 @@ class Formula(object):
                 for parameter, value in parameters.items():
                     code = code.replace(parameter + " = None", parameter + " = " + str(value))
                 return code
-        if formulaType == "pi":
+        elif formulaType == "pi":
             with open('formula_pi_notation.txt', 'r') as file:
                 code = file.read()
                 for parameter, value in parameters.items():
                     code = code.replace(parameter + " = None", parameter + " = " + str(value))
                 return code
+        elif formulaType == "permutation":
+            with open('formula_pi_notation.txt', 'r') as file:
+                code = file.read()
+                for parameter, value in parameters.items():
+                    code = code.replace(parameter + " = None", parameter + " = " + str(value))
+                return code
+        elif formulaType == "combination":
+            with open('formula_pi_notation.txt', 'r') as file:
+                code = file.read()
+                for parameter, value in parameters.items():
+                    code = code.replace(parameter + " = None", parameter + " = " + str(value))
+                return code
+        
     
     def changeParameterNames(self, parameters):
         for old_name, new_name in parameters.items():
--- test_formula.py
+++ test_formula.py
@@ -1,7 +1,7 @@
 from formula import Formula
 
 formulaType = "sigma"
-parameters = {"_i_":0, "_n_":10, "_function_":"lambda k: k", "_result_":"0"}
-variableNames = {"_i_":"i", "_n_":"n", "_function_":"function", "_k_":"k", "_result_":"result"}
+parameters = {"_i_":0, "_n_":10, "_result_":"0"}
+variableNames = {"_i_":"i", "_n_":"n", "_k_":"k", "_result_":"result"}
 myFormula = Formula(formulaType, parameters, variableNames)
 myFormula.printCode()
