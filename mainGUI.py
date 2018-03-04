4e7470033380be5f9d20a152ae68e331eb3dd027
Micah Brewer <micah@wless-user-172019249194.temp.wsu.edu>
2018-03-03 17:08:26 -0800

    Added a pannel

--- mainGUI.py
+++ mainGUI.py
@@ -3,11 +3,11 @@ import wx
 class window(wx.Frame):
     def __init__(self, *args, **kwargs):
         super(window, self).__init__(*args, **kwargs)
-        self.Centre()
+        self.Center()
         self.Show()
         
-        self.panel = wx.Panel(self)
-        self.panel.SetBackgroundColour("Green")
+        self.panel = wx.Panel(self, size=(800,500))
+        self.panel.SetBackgroundColour("Cream")
         
 def main():
     app = wx.App()
