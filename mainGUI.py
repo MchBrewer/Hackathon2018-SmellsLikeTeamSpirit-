25d40679a4d5401beba7615cdf3af21b3587ea9c
Micah Brewer <micah@wless-user-172019249194.temp.wsu.edu>
2018-03-04 00:45:04 -0800

    Split into three panels Added event for sigma button

--- mainGUI.py
+++ mainGUI.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages')
 import wx
 
 class window(wx.Frame):
@@ -6,14 +8,22 @@ class window(wx.Frame):
         self.Center()
         self.Show()
         
-        self.panel      = wx.Panel  (self,  size=(800,500))
-        self.panel.SetBackgroundColour("Cream")
+        self.panel  = wx.Panel  (self,  size=(800,250), pos=(0,0),      style=(wx.SUNKEN_BORDER))
+        self.Input  = wx.Panel  (self,  size=(400,250), pos=(0,250),    style=(wx.SUNKEN_BORDER))
+        self.Output = wx.Panel  (self,  size=(400,250), pos=(400,250),  style=(wx.SUNKEN_BORDER))
         
-        self.sigBTN     = wx.Button (self.panel,    label="sigma",  pos=(10,10),    size=(75,25))
-        self.piBTN      = wx.Button (self.panel,    label="pi",     pos=(10, 35),   size=(75,25))
+        self.panel.SetBackgroundColour("grey")
         
-       #self.funcInput  = wx.TextEntryDialog    (self.panel,    "Promt",    "title",            "DefaultText")
-        self.fuctInput  = wx.TextCtrl(self.panel,   -1,             "f(x)",         pos=(10,55),    size=(150,50))  
+        self.sumBTN     = wx.Button (self.panel,    label="sigma",  pos=(10,10),    size=(75,25))
+        self.piBTN      = wx.Button (self.panel,    label="pi",     pos=(85, 10),   size=(75,25))
+        
+        self.sumBTN.Bind(wx.EVT_BUTTON, self.sumClick)
+        
+        
+    def sumClick(self, evt):
+        self.sumInTop   = wx.TextCtrl(self.Input, -1,   "1",         pos=(10,55),    size=(25,25))
+        self.fuctInput  = wx.TextCtrl(self.Input, -1,   "2",         pos=(20,85),      size=(150,25))
+        self.sumInBtm   = wx.TextCtrl(self.Input, -1,   "3",         pos=(10,115),    size=(25,25))
         
         
 def main():
